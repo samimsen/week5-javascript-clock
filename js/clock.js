c048a749c938bb5791ad0a0e144eb09221d42374
Samim Sen <samim.sen@hotmail.com>
2025-11-06 12:06:00 +0300

    yorum satirlari eklendi

--- js/clock.js
+++ js/clock.js
@@ -1,18 +1,23 @@
+//elementler secildi
 const nameSpan = document.getElementById("myName");
 const clock = document.getElementById("myClock");
 
+//isim alındı ve ilgili tag içerisine yazıldı
 const userName = prompt("İsminizi girin:");
 nameSpan.innerText = userName;
 
+//saat ve gün gösterilecek fonksiyon
 const showTime = () => {
-    const date = new Date();
-    const currentHour = date.toLocaleTimeString();
-    const currentDay = date.toLocaleDateString("tr-TR", { weekday: "long" });
-    clock.innerText = `${currentHour} ${currentDay}`
+    const date = new Date(); // date objesi
+    const currentHour = date.toLocaleTimeString(); //fonksiyon calistigi andaki saat 
+    const currentDay = date.toLocaleDateString("tr-TR", { weekday: "long" }); // fonksiyon calistigi andaki gun
+    clock.innerText = `${currentHour} ${currentDay}` //secilen elemente yazildi
 }
 
+//fonksiyon her saniye tekrar çağırılacak
 setInterval(showTime, 1000);
 
+//baslangıc olarak cagirildi
 showTime()
 
 
