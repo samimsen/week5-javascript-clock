0676961806470a9ce5cee0370261590fe7e14c5f
Samim Sen <samim.sen@hotmail.com>
2025-11-06 12:27:59 +0300

    ek ozellik eklendi

--- js/clock.js
+++ js/clock.js
@@ -11,6 +11,20 @@ const showTime = () => {
     const date = new Date(); // date objesi
     const currentHour = date.toLocaleTimeString(); //fonksiyon calistigi andaki saat 
     const currentDay = date.toLocaleDateString("tr-TR", { weekday: "long" }); // fonksiyon calistigi andaki gun
+
+    //ek ozellik - saate ve gun dilimine göre arka plan ve yazı rengi degisimi
+    if (currentHour < 12) {
+        document.body.style.backgroundColor = "#FFFAE6"
+        document.body.style.color = "#5E4B36";
+    } else if (currentHour < 18) {
+        document.body.style.backgroundColor = "#E7F5FF";
+        document.body.style.color = "#003049";
+    }
+    else {
+        document.body.style.backgroundColor = "#1E1E2F";
+        document.body.style.color = "#EDEDED";
+    }
+
     clock.innerText = `${currentHour} ${currentDay}` //secilen elemente yazildi
 }
 
